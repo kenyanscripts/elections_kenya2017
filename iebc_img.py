f6d6dd90b1770df10f2a320c32cddecb8223db7a
Kenyan Scripts <29949457+kenyanscripts@users.noreply.github.com>
2017-10-26 23:18:24 +0300

    Update iebc_img.py

--- iebc_img.py
+++ iebc_img.py
@@ -6,11 +6,10 @@ import requests
 import csv
 
 dlm = "_"
-dest_folder = "c:\\temp\\iebc\\images\\"
-#dest_folder = "iebc/images/"
+dest_folder = "iebc/images/"
 url_base = 'https://forms.iebc.or.ke/download/39696-1_1_'
 
-with open('in.csv') as csvfile:
+with open('PollingStationCodes.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter='|')
     next (readCSV)
     for row in readCSV:      
@@ -22,10 +21,9 @@ with open('in.csv') as csvfile:
         print ("Downloading form 34A from " + str(st_cd)   + " - " + st_name)
         fileName = str(st_cd[0:0+3]) + dlm + str(st_cd[3:3+3]) + dlm + str(st_cd[6:6+4]) + dlm + str(st_cd[10:10+3]) + dlm + str(st_cd[13:13+2]) + ".jpeg"
         url = url_base + fileName
+        
         dest = dest_folder + fileName
 
-        print (url)
-        
         req = requests.get(url)
         file = open(dest, 'wb')
         for chunk in req.iter_content(100000):
